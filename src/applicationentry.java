3e27523ba91675b497cf579cf99ecabd682c4567
Joanna Jacob <joannajacob910@gmail.com>
2026-02-27 09:03:01 +0530

    Added UC1 Palindrome Checker App in src folder

---
+++ src/applicationentry.java
@@ -0,0 +1,22 @@
+public class applicationentry {
+    public static void main(String[] args) {
+
+        // Display application details
+        System.out.println("====================================");
+        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("====================================");
+
+        // Application information
+        System.out.println("Application Name  : Palindrome Checker");
+        System.out.println("Application Version : 1.0");
+
+        System.out.println("------------------------------------");
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("This program will help you check whether a word");
+        System.out.println("or number is a palindrome.");
+        System.out.println("------------------------------------");
+
+        // Flow control message
+        System.out.println("Program is ready to continue to the next use case...");
+    }
+}
